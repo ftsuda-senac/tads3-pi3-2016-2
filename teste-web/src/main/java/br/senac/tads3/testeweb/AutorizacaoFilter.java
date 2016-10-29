b1edf883ae2979f72ab42a07a26aa199cfc517f1
fernando.tsuda <fernando.tsuda@CAS555092W0724.senacsp.edu.br>
2016-10-28 22:58:23 -0200

    Correção dos erros de redirect infinito

--- teste-web/src/main/java/br/senac/tads3/testeweb/AutorizacaoFilter.java
+++ teste-web/src/main/java/br/senac/tads3/testeweb/AutorizacaoFilter.java
@@ -6,9 +6,6 @@
 package br.senac.tads3.testeweb;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -24,71 +21,10 @@ import javax.servlet.http.HttpSession;
  *
  * @author fernando.tsuda
  */
-@WebFilter(filterName = "AutorizacaoFilter", urlPatterns = {"/*"})
+@WebFilter(filterName = "AutorizacaoFilter",
+	servletNames = {"FormularioServlet", "TesteServlet"},
+	urlPatterns = {"/protegido/*"})
 public class AutorizacaoFilter implements Filter {
-
-  private static final boolean debug = true;
-
-  // The filter configuration object we are associated with.  If
-  // this value is null, this filter instance is not currently
-  // configured. 
-  private FilterConfig filterConfig = null;
-
-  public AutorizacaoFilter() {
-  }
-
-  private void doBeforeProcessing(ServletRequest request, ServletResponse response)
-	  throws IOException, ServletException {
-    if (debug) {
-      log("AutorizacaoFilter:DoBeforeProcessing");
-    }
-
-    // Write code here to process the request and/or response before
-    // the rest of the filter chain is invoked.
-    // For example, a logging filter might log items on the request object,
-    // such as the parameters.
-	/*
-     for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
-     String name = (String)en.nextElement();
-     String values[] = request.getParameterValues(name);
-     int n = values.length;
-     StringBuffer buf = new StringBuffer();
-     buf.append(name);
-     buf.append("=");
-     for(int i=0; i < n; i++) {
-     buf.append(values[i]);
-     if (i < n-1)
-     buf.append(",");
-     }
-     log(buf.toString());
-     }
-     */
-  }
-
-  private void doAfterProcessing(ServletRequest request, ServletResponse response)
-	  throws IOException, ServletException {
-    if (debug) {
-      log("AutorizacaoFilter:DoAfterProcessing");
-    }
-
-	// Write code here to process the request and/or response after
-    // the rest of the filter chain is invoked.
-    // For example, a logging filter might log the attributes on the
-    // request object after the request has been processed. 
-	/*
-     for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
-     String name = (String)en.nextElement();
-     Object value = request.getAttribute(name);
-     log("attribute: " + name + "=" + value.toString());
-
-     }
-     */
-    // For example, a filter might append something to the response.
-	/*
-     PrintWriter respOut = new PrintWriter(response.getWriter());
-     respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
-     */
-  }
 
   /**
    *
@@ -99,6 +35,7 @@ public class AutorizacaoFilter implements Filter {
    * @exception IOException if an input/output error occurs
    * @exception ServletException if a servlet error occurs
    */
+  @Override
   public void doFilter(ServletRequest request, ServletResponse response,
 	  FilterChain chain)
 	  throws IOException, ServletException {
@@ -106,20 +43,6 @@ public class AutorizacaoFilter implements Filter {
     // 1) FAZER CAST DOS OBJETOS REQUEST E RESPONSE
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
-    
-    // Verifica se está acessando página de login
-    try {
-      if (httpRequest.getRequestURI().endsWith("/login") ||
-	      httpRequest.getRequestURI().endsWith("/erro-login.jsp") ||
-	      httpRequest.getRequestURI().endsWith("/erro-nao-autorizado.jsp")) {
-	chain.doFilter(request, response);
-      }
-    } catch (Throwable t) {
-      // If an exception is thrown somewhere down the filter chain,
-      // we still want to execute our after processing, and then
-      // rethrow the problem after that.
-      t.printStackTrace();
-    }
 
     // 2) TENTAR RECUPERAR USUARIO DA SESSAO
     HttpSession sessao = httpRequest.getSession(false);
@@ -144,7 +67,7 @@ public class AutorizacaoFilter implements Filter {
 	chain.doFilter(request, response);
       } else {
 	// NÃO PODE ACESSAR. APRESENTA TELA DE ERRO
-	httpResponse.sendRedirect(httpRequest.getContextPath() 
+	httpResponse.sendRedirect(httpRequest.getContextPath()
 		+ "/erro-nao-autorizado.jsp");
       }
 
@@ -178,99 +101,19 @@ public class AutorizacaoFilter implements Filter {
   }
 
   /**
-   * Return the filter configuration object for this filter.
-   */
-  public FilterConfig getFilterConfig() {
-    return (this.filterConfig);
-  }
-
-  /**
-   * Set the filter configuration object for this filter.
-   *
-   * @param filterConfig The filter configuration object
-   */
-  public void setFilterConfig(FilterConfig filterConfig) {
-    this.filterConfig = filterConfig;
-  }
-
-  /**
    * Destroy method for this filter
    */
+  @Override
   public void destroy() {
+
   }
 
   /**
    * Init method for this filter
    */
-  public void init(FilterConfig filterConfig) {
-    this.filterConfig = filterConfig;
-    if (filterConfig != null) {
-      if (debug) {
-	log("AutorizacaoFilter:Initializing filter");
-      }
-    }
-  }
-
-  /**
-   * Return a String representation of this object.
-   */
   @Override
-  public String toString() {
-    if (filterConfig == null) {
-      return ("AutorizacaoFilter()");
-    }
-    StringBuffer sb = new StringBuffer("AutorizacaoFilter(");
-    sb.append(filterConfig);
-    sb.append(")");
-    return (sb.toString());
-  }
+  public void init(FilterConfig filterConfig) {
 
-  private void sendProcessingError(Throwable t, ServletResponse response) {
-    String stackTrace = getStackTrace(t);
-
-    if (stackTrace != null && !stackTrace.equals("")) {
-      try {
-	response.setContentType("text/html");
-	PrintStream ps = new PrintStream(response.getOutputStream());
-	PrintWriter pw = new PrintWriter(ps);
-	pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
-
-	// PENDING! Localize this for next official release
-	pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
-	pw.print(stackTrace);
-	pw.print("</pre></body>\n</html>"); //NOI18N
-	pw.close();
-	ps.close();
-	response.getOutputStream().close();
-      } catch (Exception ex) {
-      }
-    } else {
-      try {
-	PrintStream ps = new PrintStream(response.getOutputStream());
-	t.printStackTrace(ps);
-	ps.close();
-	response.getOutputStream().close();
-      } catch (Exception ex) {
-      }
-    }
-  }
-
-  public static String getStackTrace(Throwable t) {
-    String stackTrace = null;
-    try {
-      StringWriter sw = new StringWriter();
-      PrintWriter pw = new PrintWriter(sw);
-      t.printStackTrace(pw);
-      pw.close();
-      sw.close();
-      stackTrace = sw.getBuffer().toString();
-    } catch (Exception ex) {
-    }
-    return stackTrace;
-  }
-
-  public void log(String msg) {
-    filterConfig.getServletContext().log(msg);
   }
 
 }
